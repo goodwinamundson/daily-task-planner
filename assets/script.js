add77f01ea556d453e12f8bf84b2cd23eed4c252
goodwinamundson <goodwinamundson@gmail.com>
2022-01-31 11:33:38 +1200

    add readme file

--- assets/script.js
+++ assets/script.js
@@ -2,22 +2,23 @@ var today = new Date();
 var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
 // to display the day and time at the top of the page
 document.getElementById('currentDay').innerHTML = date;
+// can also be written using moment.js as: $("#currentDay").text(moment().format('dddd MMMM Do YYYY'));)
 
 
 var saveBtn = $(".saveBtn");
 
 // each time slot's color chanes depending on  if it is in the past, present, or future
 function timeSlotColor() {
-    var hour = today.getHours()
+    var time = today.getHours()
 
     $(".time-block").each(function() {
         var currentTime = parseInt($(this).attr("id"));
 
         // console.log(this); //each time-block
 
-        if (currentTime > hour) {
+        if (currentTime > time) {
             $(this).addClass("future");
-        } else if (currentTime === hour) {
+        } else if (currentTime === time) {
             $(this).addClass("present");
         } else {
             $(this).addClass("past");
